e258f3f433b0d2122b173656df7124c8268bc657
xiaoxunyao <huchengyea@163.com>
2022-06-27 21:35:41 +0800

    fix: update core

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -24,21 +24,18 @@ export default defineConfig({
       }),
   ],
   build: {
+    watch: {
+      include: ["src/**/*.*"],
+    },
+    outDir: "./dist",
     lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: '@elonehoo/vue-mac-keyboard',
-      // the proper extensions will be added
-      fileName: 'index'
+      formats: ["es"],
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "index",
+      fileName: () => "index.js",
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
+      external: Object.keys(require("./package.json").peerDependencies),
+    },
+  },
 })
