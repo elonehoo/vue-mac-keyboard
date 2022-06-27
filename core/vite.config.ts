340bee46c2af38063eac37e6846646913d1b2573
xiaoxunyao <huchengyea@163.com>
2022-06-27 21:58:39 +0800

    fix: update

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -14,20 +14,4 @@ export default defineConfig({
       reactivityTransform: true,
     }),
   ],
-  build: {
-    rollupOptions: {
-      // 请确保外部化那些你的库中不需要的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    lib: {
-      entry: './src/index.ts',
-      name: 'index',
-    },
-  },
 })
