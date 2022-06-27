5077bbdb14e21aeb9cfc77b4093fd6b6007a2b36
xiaoxunyao <huchengyea@163.com>
2022-06-27 22:11:41 +0800

    fix: update

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -14,4 +14,22 @@ export default defineConfig({
       reactivityTransform: true,
     }),
   ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'vue-mac-keyboard',
+      // the proper extensions will be added
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
